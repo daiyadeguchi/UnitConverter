484e4b7f758a7f44c2ab6d3e5f6654030aa6eecd
daiyadeguchi <daiyadeguchi@gmail.com>
2022-08-30 08:03:21 +0900

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Daiya Deguchi on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
